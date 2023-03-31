74bdd044d51e0ff7e5e18b39a08edd57b4ac0733
jirenl <128604368+jirenl@users.noreply.github.com>
2023-03-31 07:05:13 +0200

    Kommentare inzugefuegt

--- nur_cam.py
+++ nur_cam.py
@@ -12,10 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Main script to run image classification."""
+
+#Main-Skript für die Bild-Klassifikation
 
 import drivers
-import argparse
+import argparse         #System-relevante Imports
 import sys
 
 from time import sleep
@@ -24,10 +25,10 @@ import time
 
 import cv2
 from tflite_support.task import core
-from tflite_support.task import processor
+from tflite_support.task import processor       #Imports für die Objekterkennung
 from tflite_support.task import vision
 
-# Visualization parameters
+# Fenster für die Ergebnisvisualisierung
 _ROW_SIZE = 20  # pixels
 _LEFT_MARGIN = 24  # pixels
 _PADDING = 10
@@ -37,25 +38,24 @@ _FONT_THICKNESS = 1
 _FPS_AVERAGE_FRAME_COUNT = 10
 
 def run(model: str, max_results: int, score_threshold: float, num_threads: int,
-        enable_edgetpu: bool, camera_id: int, width: int, height: int) -> None:
-  """Continuously run inference on images acquired from the camera.
+        enable_edgetpu: bool, camera_id: int, width: int, height: int) -> None:         #deklariert argumente ohne inhalt (wird im unteren teil gefüllt)
 
-  Args:
-      model: Name of the TFLite image classification model.
-      max_results: Max of classification results.
-      score_threshold: The score threshold of classification results.
-      num_threads: Number of CPU threads to run the model.
-      enable_edgetpu: Whether to run the model on EdgeTPU.
-      camera_id: The camera id to be passed to OpenCV.
-      width: The width of the frame captured from the camera.
-      height: The height of the frame captured from the camera.
-  """
+ #   Argumentenbeschreibeung: ###########
+ #     model: Name des TFLite-Models.
+ #     max_results: Maximale Anzahl von Bilderkennungsergebnisse.
+ #     score_threshold: The score threshold of classification results.
+ #     num_threads: Anzahl der CPU Threads für das betreiben der Modelle.
+ #     enable_edgetpu: Whether to run the model on EdgeTPU.
+ #     camera_id: Die Kamera-ID wird an OpenCV gegeben.
+ #     width: Die Breite für das Fenster der Kamera.
+ #     height: Die Höhe für das Fenster der Kamera.
+ ##############################
 
-  # Initialize the image classification model
+  # Initialisiert die Bild-Klassifikation
   base_options = core.BaseOptions(
       file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
 
-  # Enable Coral by this setting
+  #"Aktiviert" Die Objekt-Erkennung
   classification_options = processor.ClassificationOptions(
       max_results=max_results, score_threshold=score_threshold)
   options = vision.ImageClassifierOptions(
@@ -63,58 +63,58 @@ def run(model: str, max_results: int, score_threshold: float, num_threads: int,
 
   classifier = vision.ImageClassifier.create_from_options(options)
 
-  # Variables to calculate FPS
+  # Benötigte Variablen für die FPS-Anzeige (Bilder pro Sekunde)
   counter, fps = 0, 0
   start_time = time.time()
 
-  # Start capturing video input from the camera
+  # Startet die Videoaufnahmeingabe von der Kamera 
   cap = cv2.VideoCapture(camera_id)
   cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
   print("Es geht los mit dem Scan! ")
-  # Continuously capture images from the camera and run inference
+  # Eine While-Schleife soll solange neue Bilder machen bis das Programm mit "ESC" geschlossen wird
   while cap.isOpened():
     #sleep(1)
     success, image = cap.read()
     if not success:
       sys.exit(
-          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+          'FEHLER: Das Lesen der Kamera ist nicht moeglich. Bitte verifizieren Sie die Kamera-Einstellungen.'
       )
 
-    counter += 1
-    image = cv2.flip(image, 1)
+    counter += 1        #zählt die Programmabläufe mit
+    image = cv2.flip(image, 1)  #spiegelt die Kamera, damit die Ausgabe wie eine "Spiegel" scheint
 
-    # Convert the image from BGR to RGB as required by the TFLite model.
+    # Konvertiert das Bild von BRG zu RGB, weil TFLite dies für die Verarbeitung benötigt.
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Create TensorImage from the RGB image
+    # Erstellt ein TensorImage aus dem RGB BIld
     tensor_image = vision.TensorImage.create_from_array(rgb_image)
-    # List classification results
+    # Listet alle möglichen Klassifikationsergebnisse ein
     categories = classifier.classify(tensor_image)
     
-    # Show classification results on the image
+    # Zeigt alle möglichen Klassifikationsergebnisse auf der Anzeige im Fenster
     for idx, category in enumerate(categories.classifications[0].categories):
       category_name = category.category_name
       score = round(category.score, 2)
-      result_text = category_name + ' (' + str(round(score*100,2)) + '%)'  #gibt den Objektnamen aus
+      result_text = category_name + ' (' + str(round(score*100,2)) + '%)'  #gibt den Objektnamen mit Prozentwahrscheinlichkeit aus
       text_location = (_LEFT_MARGIN, (idx + 2) * _ROW_SIZE)
       cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
                   _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
 
 
-    # Calculate the FPS
+    # Errechnet die wahrscheinliche FPS
     if counter % _FPS_AVERAGE_FRAME_COUNT == 0:
       end_time = time.time()
       fps = _FPS_AVERAGE_FRAME_COUNT / (end_time - start_time)
       start_time = time.time()
 
-    # Show the FPS
+    # zeigt die FPS an
     fps_text = 'FPS = ' + str(int(fps))
     text_location = (_LEFT_MARGIN, _ROW_SIZE)
     cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
                 _FONT_SIZE, _TEXT_COLOR, _FONT_THICKNESS)
 
-    # Stop the program if the ESC key is pressed.
+    # Stoppt das Programm wenn der Benutzer "ESC" gedrückt hat
     if cv2.waitKey(1) == 27:  #27 ist für den ACII-Wert ESCAPE
       break
     cv2.imshow('image_classification', image)
@@ -167,10 +167,10 @@ def main():
   args = parser.parse_args()
 
   run(args.model, int(args.maxResults),
-      args.scoreThreshold, int(args.numThreads), bool(args.enableEdgeTPU),
+      args.scoreThreshold, int(args.numThreads), bool(args.enableEdgeTPU),              #siehe oben: setzt die Argumenten mit den initialisierten Argumenten ein 
       int(args.cameraId), args.frameWidth, args.frameHeight)
     
     
-if __name__ == '__main__':
+if __name__ == '__main__':      #die Hauptfunktion wird erst ausgeführt wenn die main nicht verändert wurde von Imports.
   main()
 
